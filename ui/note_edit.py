d286475ee13010f600715960f86cc69ae8d2df28
OutlyingWest <alexeybuv7@gmail.com>
2022-12-14 14:11:00 +0300

    minimal workable version

--- ui/note_edit.py
+++ ui/note_edit.py
@@ -1,9 +1,33 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtCore import Slot, Signal
+from PySide6.QtWidgets import QWidget, QDialog
 from ui.ui_pyside_base.ui_note_edit import Ui_NoteEdit
 
 
 class NoteEditWindow(QWidget):
+    ok_signal = Signal(str)
+
     def __init__(self):
         super().__init__()
         self.ui = Ui_NoteEdit()
         self.ui.setupUi(self)
+        self.is_deleted = False
+        self.ui.OkButton.clicked.connect(self.press_ok)
+        self.ui.CancelButton.clicked.connect(self.press_cancel)
+
+    @Slot()
+    def press_ok(self):
+        title_text = self.ui.HeaderNoteLineEdit.text()
+        self.ok_signal.emit(title_text)
+        self.hide()
+
+    @Slot()
+    def press_cancel(self):
+        # Signal to note plate that note edit object was deleted
+        self.is_deleted = True
+        # Close window
+        self.close()
+        # Planning note edit window object deletion
+        self.deleteLater()
+
+
+
--- ui/note_plate.py
+++ ui/note_plate.py
@@ -1,11 +1,11 @@
 from PySide6.QtCore import Slot, Signal
 from PySide6.QtWidgets import QWidget
 from ui.ui_pyside_base.ui_note_plate import Ui_NotePlate
+from ui.note_edit import NoteEditWindow
 
 
 class NotePlateWidget(QWidget):
     """ Class of note plate in scrolling area in main window """
-    write = Signal(int)
     delete = Signal(int)
 
     def __init__(self, id_widget: int):
@@ -16,16 +16,30 @@ class NotePlateWidget(QWidget):
         self.id_widget = id_widget
         # Set title to note plate in scrolling area
         self.ui.TitleButton.setText("My title")
+        # Object of note edit modal window
+        self.note_edit_window = None
         # Connections to buttons on plate
-        self.ui.DelButton.clicked.connect(self.press_write_note)
+        self.ui.TitleButton.clicked.connect(self.press_write_note)
         self.ui.DelButton.clicked.connect(self.press_del)
 
     @Slot()
     def press_write_note(self):
         """ Open a note edit window """
-        pass
+        note_edit_widget = self.sender()
+        if not self.note_edit_window or self.note_edit_window.is_deleted:
+            self.note_edit_window = NoteEditWindow()
+            self.note_edit_window.ok_signal.connect(self.update_title)
+            self.note_edit_window.show()
+
+        else:
+            self.note_edit_window.show()
 
     @Slot()
     def press_del(self):
         """ Handle a press on delete button on plate """
         self.delete.emit(self.id_widget)
+
+    @Slot(str)
+    def update_title(self, new_title: str):
+        if new_title:
+            self.ui.TitleButton.setText(new_title)
