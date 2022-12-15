441529857643923660f843e6eb562172e1d9ca99
OutlyingWest <alexeybuv7@gmail.com>
2022-12-15 22:16:07 +0300

    refactor notebook.py

--- notebook.py
+++ notebook.py
@@ -3,21 +3,14 @@ import sys
 from PySide6.QtWidgets import QApplication
 
 from ui.mainwindow import MainWindow
-from ui.note_edit import NoteEditWindow
-from ui.note_plate import NotePlateWidget
-
 from data.notes_handler import set_paths, load_notes_data
 
 
 def main():
     load_config()
     app = QApplication()
-    # window_ne = NoteEditWindow()
-    # window_np = NotePlateWidget(1)
     main_window = MainWindow()
 
-    # window_ne.show()
-    # window_np.show()
     main_window.show()
     sys.exit(app.exec())
 
